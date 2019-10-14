3c5c76b5384dea8f0e5d0da1c4e82cfc1892e90d
Pablo Lacasta <bapalnas@gmail.com>
2019-10-14 17:47:37 +0200

    store function in LibrosController

--- app/Http/Controllers/LibrosController.php
+++ app/Http/Controllers/LibrosController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class librosController extends Controller
 {
-    //
+    public function store(Request $request, Libros $libro){
+        $libro = new Libro;
+        $libro->nombre = $request->nombre;
+        $libro->precio = $request->precio;
+        $libro->categoria = $request->categoria;
+
+        $libro->save();
+
+        return $libro;
+    }
 }
