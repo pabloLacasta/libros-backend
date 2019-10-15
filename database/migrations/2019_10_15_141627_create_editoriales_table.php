a562faf0145135caafa5708536af174cb67a551b
Pablo Lacasta <bapalnas@gmail.com>
2019-10-15 16:20:24 +0200

    migration editoriales

---
+++ database/migrations/2019_10_15_141627_create_editoriales_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEditorialesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('editoriales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('editoriales');
+    }
+}
